b4235422db345e5b536af5622e08ef84a30d6aa3
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2025-02-16 22:08:13 +0300

    Update wsgi.py

--- backend/kairos/wsgi.py
+++ backend/kairos/wsgi.py
@@ -1,1 +1,17 @@
+"""
+WSGI config for Kairos project.
 
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+# Set the default Django settings module for the 'wsgi' application.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kairos.settings')
+
+# Get the WSGI application for the project
+application = get_wsgi_application()
