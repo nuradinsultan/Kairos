602b428074c7ada3311ae58cfd055bc118910b47
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2025-03-19 19:27:38 +0300

    Create views.py

---
+++ backend/apps/authentication/views.py
@@ -0,0 +1,16 @@
+# apps/authentication/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from .models import User
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({"refresh": str(refresh), "access": str(refresh.access_token)})
+        return Response({"error": "Invalid Credentials"}, status=400)
