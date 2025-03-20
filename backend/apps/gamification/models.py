caea6087c305a4faf879705b73705b15264e6e08
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2025-03-20 18:38:59 +0300

    Update models.py

--- backend/apps/gamification/models.py
+++ backend/apps/gamification/models.py
@@ -1,1 +1,8 @@
+# apps/gamification/models.py
+from django.db import models
+from apps.authentication.models import User
 
+class KenoEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    numbers = models.CharField(max_length=50)  # Comma-separated numbers
+    entry_time = models.DateTimeField(auto_now_add=True)
