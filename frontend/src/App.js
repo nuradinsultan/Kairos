906be896715cf79fda135539a622584ae6aff413
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2025-03-20 18:17:33 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,20 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
+import { AuthProvider } from './context/AuthContext';
+import { PortfolioProvider } from './context/PortfolioContext';
 
+function App() {
+  return (
+    <AuthProvider>
+      <PortfolioProvider>
+        <Router>
+          <AppRoutes />
+        </Router>
+      </PortfolioProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
