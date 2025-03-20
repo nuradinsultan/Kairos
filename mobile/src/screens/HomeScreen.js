dfb40feaf9af596b506ac28b0c7a684e8fc1ae53
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2025-03-20 18:12:11 +0300

    Update HomeScreen.js

--- mobile/src/screens/HomeScreen.js
+++ mobile/src/screens/HomeScreen.js
@@ -1,1 +1,11 @@
+// src/screens/HomeScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
 
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Welcome to Kairos Trading App</Text>
+    </View>
+  );
+}
