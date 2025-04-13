7eebe0b898fe0505c29de4f65e4eba1c15df32e8
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2025-04-13 20:09:17 +0300

    Update urls.py

--- backend/kairos/urls.py
+++ backend/kairos/urls.py
@@ -1,6 +1,8 @@
-# backend/kairos_backend/urls.py
-from django.urls import include, path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/trading/', include('apps.trading.urls')),
     path('api/auth/', include('apps.authentication.urls')),
 ]
