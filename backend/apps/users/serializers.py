6dda16fe008123496584995637cb1362a55946e4
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2025-04-14 18:43:42 +0300

    Update serializers.py

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,24 +1,32 @@
 from rest_framework import serializers
 from .models import User
-from django.contrib.auth.password_validation import validate_password
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
-    
+        fields = ['id', 'username', 'email', 'phone_number', 'is_active']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'username': {'required': True}
+        }
+
     def create(self, validated_data):
-        user = User.objects.create_user(
-            email=validated_data['email'],
-            username=validated_data['username'],
-            password=validated_data['password']
-        )
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
         return user
 
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'is_verified']
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+        
+        if not username or not password:
+            raise serializers.ValidationError("Both username and password are required")
+        
+        return data
