37d80a19df8595526eddf17a0ca48424993c7437
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2025-04-14 18:57:08 +0300

    Update urls.py

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,1 +1,16 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import (
+    UserListView,
+    UserDetailView,
+    RegisterView,
+    CustomTokenObtainPairView
+)
 
+urlpatterns = [
+    path('', UserListView.as_view(), name='user-list'),
+    path('me/', UserDetailView.as_view(), name='user-detail'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
