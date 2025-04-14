1df099e23e8fad30686672e6b0577b95814892fa
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2025-04-14 20:43:04 +0300

    Update Admin.py

--- backend/apps/users/Admin.py
+++ backend/apps/users/Admin.py
@@ -1,27 +1,68 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import User
 
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'phone_number', 'is_staff', 'is_verified')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_verified')
+    """Best Practice: Comprehensive admin interface"""
+    
+    list_display = (
+        'username', 'email', 'phone_number', 
+        'is_active', 'is_staff', 'is_verified',
+        'date_joined'
+    )
+    list_filter = (
+        'is_active', 'is_staff', 'is_superuser', 
+        'is_verified', 'date_joined'
+    )
     search_fields = ('username', 'email', 'phone_number')
-    ordering = ('username',)
+    ordering = ('-date_joined',)
+    filter_horizontal = ('groups', 'user_permissions',)
+    readonly_fields = ('date_joined', 'last_login')
     
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Contact Info', {'fields': ('email', 'phone_number')}),
-        ('Permissions', {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'is_verified', 'groups', 'user_permissions'),
+        (_('Contact Info'), {
+            'fields': ('email', 'phone_number', 'language')
         }),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (_('Permissions'), {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+                'is_verified', 'groups', 'user_permissions'
+            ),
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login', 'date_joined')
+        }),
     )
     
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'phone_number', 'password1', 'password2'),
+            'fields': (
+                'username', 'email', 'phone_number',
+                'password1', 'password2', 'language'
+            ),
         }),
     )
-
-admin.site.register(User, CustomUserAdmin)
+    
+    actions = ['activate_users', 'deactivate_users']
+    
+    def activate_users(self, request, queryset):
+        """Best Practice: Custom admin action"""
+        updated = queryset.update(is_active=True)
+        self.message_user(
+            request,
+            f'Activated {updated} users'
+        )
+    activate_users.short_description = "Activate selected users"
+    
+    def deactivate_users(self, request, queryset):
+        """Best Practice: Custom admin action"""
+        updated = queryset.update(is_active=False)
+        self.message_user(
+            request,
+            f'Deactivated {updated} users'
+        )
+    deactivate_users.short_description = "Deactivate selected users"
